d600b0ea80a1b7f85cdd480b7e98f75135dc8923
Viktor Mendonck <viktor.mendonck@gmail.com>
2024-03-03 12:42:57 +0100

    3d transform for some stupid ahh reason

--- Minigin/Transform.cpp
+++ Minigin/Transform.cpp
@@ -1,15 +1,64 @@
 #include "Transform.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <stdexcept>
 
-void dae::Transform::SetPosition(const float x, const float y, const float z)
+
+
+void dae::Transform::Translate(const glm::vec3& delta)
 {
-	m_position.x = x;
-	m_position.y = y;
-	m_position.z = z;
+	m_Position += delta;
+	m_IsDirty = true;
 }
 
-void dae::Transform::SetPosition(float x, float y)
+void dae::Transform::SetPosition(const glm::vec3& position)
 {
-	m_position.x = x;
-	m_position.y = y;
-	m_position.z = 0;
+	m_Position = position;
+	m_IsDirty = true;
+}
+
+void dae::Transform::Rotate(const glm::vec3& delta)
+{
+	m_Rotation += delta;
+	m_IsDirty = true;
+}
+
+void dae::Transform::SetRotation(const glm::vec3& rotation)
+{
+	m_Rotation = rotation;
+	m_IsDirty = true;
+}
+
+void dae::Transform::SetScale(const glm::vec3& scale)
+{
+	m_Scale = scale;
+	m_IsDirty = true;
+}
+
+glm::mat4& dae::Transform::GetTransformationMatrix()
+{
+	if (m_IsDirty)
+		UpdateTransformationMatrix();
+	return m_TransformationMatrix;
+}
+
+void dae::Transform::UpdateTransformationMatrix()
+{
+	//assert if dirty flag is not set to true
+	throw std::runtime_error(std::string("Dirty flag not set to true"));
+
+	// Reset the dirty flag
+	m_IsDirty = false;
+
+	glm::mat4 transformationMatrix = glm::mat4(1.0f); // Start with the identity matrix
+
+	// Apply transformations
+	transformationMatrix = glm::translate(transformationMatrix, m_Position);
+
+	// Apply rotations around the x, y, and z axes
+	transformationMatrix = glm::rotate(transformationMatrix, m_Rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+	transformationMatrix = glm::rotate(transformationMatrix, m_Rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+	transformationMatrix = glm::rotate(transformationMatrix, m_Rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+
+	// Apply scaling
+	transformationMatrix = glm::scale(transformationMatrix, m_Scale);
 }
--- Minigin/Transform.h
+++ Minigin/Transform.h
@@ -1,15 +1,39 @@
 #pragma once
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Component.h"
+
 
 namespace dae
 {
 	class Transform final
 	{
 	public:
-		const glm::vec3& GetPosition() const { return m_position; }
-		void SetPosition(float x, float y, float z);
-		void SetPosition(float x, float y);
+	
+		Transform() = default;
+
+		void Translate(const glm::vec3& delta);
+		void SetPosition(const glm::vec3& position);
+		
+		void Rotate(const glm::vec3& delta);
+		void SetRotation(const glm::vec3& rotation);
+		
+		void SetScale(const glm::vec3& scale);
+
+
+		const glm::vec3& GetPosition() const { return m_Position; }
+		const glm::vec3& GetRotation() const { return m_Rotation; }
+		const glm::vec3& GetScale() const { return m_Scale; }
+
+		glm::mat4& GetTransformationMatrix();
+
 	private:
-		glm::vec3 m_position;
+		void UpdateTransformationMatrix();
+		glm::mat4 m_TransformationMatrix;
+
+		glm::vec3 m_Position{};
+		glm::vec3 m_Rotation{};
+		glm::vec3 m_Scale{ 1,1,1 };
+		bool m_IsDirty = true;
 	};
 }
